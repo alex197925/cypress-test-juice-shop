d90649e321ac8f3e7eb8cd736124256cc85ae9fa
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-04-11 15:22:35 +0200

    Test: Sign up

---
+++ cypress/e2e/signup_login.cy.js
@@ -0,0 +1,30 @@
+/** @format */
+
+/// <reference types="cypress" />
+describe("Signup & Login", () => {
+  let randomString = Math.random().toString(36).substring(7);
+  let randomPassword = Math.random() * 10;
+  let randomEmail = "auto_" + randomString + "@gmail.com";
+  const securityAnswer = "Hello World";
+
+  it("Validate signup", () => {
+    cy.visit("http://localhost:3000/#/");
+    cy.get(".cdk-overlay-backdrop").click(-50, -50, { force: true });
+    cy.get("button").contains("Account").click();
+    cy.get("#navbarLoginButton").contains("Login").click();
+    cy.get("#newCustomerLink > .primary-link")
+      .contains("Not yet a customer?")
+      .click({ force: true });
+    cy.log(randomString);
+
+    cy.get("#emailControl").type(randomEmail);
+    cy.get("#passwordControl").type(randomPassword);
+    cy.get("#repeatPasswordControl").type(randomPassword);
+
+    cy.get(".mat-select-placeholder").click();
+    cy.get("#mat-option-3 > .mat-option-text").click();
+    cy.get("#securityAnswerControl").type(securityAnswer);
+
+    cy.get("#registerButton").click();
+  });
+});

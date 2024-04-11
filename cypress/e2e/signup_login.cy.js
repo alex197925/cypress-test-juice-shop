47dae1058f5a402b8033d02ba3f3b7b968f1b034
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-04-11 15:53:51 +0200

    Test: Login new user

--- cypress/e2e/signup_login.cy.js
+++ cypress/e2e/signup_login.cy.js
@@ -3,8 +3,8 @@
 /// <reference types="cypress" />
 describe("Signup & Login", () => {
   let randomString = Math.random().toString(36).substring(7);
-  let randomPassword = Math.random() * 10;
-  let randomEmail = "auto_" + randomString + "@gmail.com";
+  let password = Math.random() * 10;
+  let email = "auto_" + randomString + "@gmail.com";
   const securityAnswer = "Hello world";
 
   it("Validate signup", () => {
@@ -17,14 +17,24 @@ describe("Signup & Login", () => {
       .click({ force: true });
     cy.log(randomString);
 
-    cy.get("#emailControl").type(randomEmail);
-    cy.get("#passwordControl").type(randomPassword);
-    cy.get("#repeatPasswordControl").type(randomPassword);
+    cy.get("#emailControl").type(email);
+    cy.get("#passwordControl").type(password);
+    cy.get("#repeatPasswordControl").type(password);
 
     cy.get(".mat-select-placeholder").click();
     cy.get("#mat-option-3 > .mat-option-text").click();
     cy.get("#securityAnswerControl").type(securityAnswer);
 
     cy.get("#registerButton").click();
+    cy.get(".mat-snack-bar-container").contains(
+      "Registration completed successfully. You can now log in."
+    );
+  });
+  it("Validate login user in to account", () => {
+    cy.get("button").contains("Account").click();
+    cy.get("#navbarLoginButton").contains("Login").click();
+    cy.get("#email").type(email);
+    cy.get("#password").type(password);
+    cy.get("#loginButton").click();
   });
 });
